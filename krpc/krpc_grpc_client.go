730381ad42505ba74d81c98dc5a845c4ceb6249d
jflyfox <zcool321@sina.com>
2020-12-11 00:04:28 +0800

    improve package krpc

--- krpc/krpc_grpc_client.go
+++ krpc/krpc_grpc_client.go
@@ -16,13 +16,30 @@ var (
 
 // NewGrpcConn creates and returns a client connection for given service `appId`.
 func (c *krpcClient) NewGrpcClientConn(appId string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	grpcClientOptions := opts
-	if len(grpcClientOptions) == 0 {
-		grpcClientOptions = DefaultGrpcClientConnOptions
+	grpcClientOptions := make([]grpc.DialOption, 0)
+	grpcClientOptions = append(grpcClientOptions, DefaultGrpcClientConnOptions...)
+	if len(opts) > 0 {
+		grpcClientOptions = append(grpcClientOptions, opts...)
 	}
-	conn, err := grpc.Dial(discovery.DefaultScheme+":///"+appId, DefaultGrpcClientConnOptions...)
+	conn, err := grpc.Dial(discovery.DefaultScheme+":///"+appId, grpcClientOptions...)
 	if err != nil {
 		return nil, err
 	}
 	return conn, nil
+}
+
+// ChainUnary creates a single interceptor out of a chain of many interceptors.
+//
+// Execution is done in left-to-right order, including passing of context.
+// For example ChainUnaryClient(one, two, three) will execute one before two before three.
+func (c *krpcClient) ChainUnary(interceptors ...grpc.UnaryClientInterceptor) grpc.DialOption {
+	return grpc.WithChainUnaryInterceptor(interceptors...)
+}
+
+// ChainStream creates a single interceptor out of a chain of many interceptors.
+//
+// Execution is done in left-to-right order, including passing of context.
+// For example ChainStreamClient(one, two, three) will execute one before two before three.
+func (c *krpcClient) ChainStream(interceptors ...grpc.StreamClientInterceptor) grpc.DialOption {
+	return grpc.WithChainStreamInterceptor(interceptors...)
 }
--- krpc/krpc_grpc_server.go
+++ krpc/krpc_grpc_server.go
@@ -50,6 +50,7 @@ func (s *krpcServer) NewGrpcServer(conf ...*GrpcServerConfig) *GrpcServer {
 	}
 	server.config.Options = append([]grpc.ServerOption{
 		s.ChainUnary(
+			server.UnaryError,
 			server.UnaryLogger,
 			server.UnaryRecover,
 		),
@@ -161,4 +162,20 @@ func (s *GrpcServer) Wait() {
 // Stop gracefully stops the server.
 func (s *GrpcServer) Stop() {
 	s.Server.GracefulStop()
+}
+
+// ChainUnary returns a ServerOption that specifies the chained interceptor
+// for unary RPCs. The first interceptor will be the outer most,
+// while the last interceptor will be the inner most wrapper around the real call.
+// All unary interceptors added by this method will be chained.
+func (s *krpcServer) ChainUnary(interceptors ...grpc.UnaryServerInterceptor) grpc.ServerOption {
+	return grpc.ChainUnaryInterceptor(interceptors...)
+}
+
+// ChainStream returns a ServerOption that specifies the chained interceptor
+// for stream RPCs. The first interceptor will be the outer most,
+// while the last interceptor will be the inner most wrapper around the real call.
+// All stream interceptors added by this method will be chained.
+func (s *krpcServer) ChainStream(interceptors ...grpc.StreamServerInterceptor) grpc.ServerOption {
+	return grpc.ChainStreamInterceptor(interceptors...)
 }
