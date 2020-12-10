50a4d6c5c88f5a4aab705a6c1592a89067cd11ab
jflyfox <zcool321@sina.com>
2020-12-11 01:17:14 +0800

    add UnaryError for krpc.Client

--- krpc/krpc_grpc_client.go
+++ krpc/krpc_grpc_client.go
@@ -1,9 +1,12 @@
 package krpc
 
 import (
+	"context"
+	"github.com/gogf/gf/errors/gerror"
 	"github.com/gogf/katyusha/balancer"
 	"github.com/gogf/katyusha/discovery"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
 var (
@@ -21,11 +24,30 @@ func (c *krpcClient) NewGrpcClientConn(appId string, opts ...grpc.DialOption) (*
 	if len(opts) > 0 {
 		grpcClientOptions = append(grpcClientOptions, opts...)
 	}
+	grpcClientOptions = append(grpcClientOptions, c.ChainUnary(
+		c.UnaryError,
+	))
 	conn, err := grpc.Dial(discovery.DefaultScheme+":///"+appId, grpcClientOptions...)
 	if err != nil {
 		return nil, err
 	}
 	return conn, nil
+}
+
+// UnaryError handles the error types converting between grpc and gerror.
+func (c *krpcClient) UnaryError(ctx context.Context, method string, req, reply interface{},
+	cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	if err != nil {
+		grpcStatus, ok := status.FromError(err)
+		if ok {
+			if code := grpcStatus.Code(); code != 0 {
+				return gerror.NewCode(int(code), grpcStatus.Message())
+			}
+			return gerror.New(grpcStatus.Message())
+		}
+	}
+	return err
 }
 
 // ChainUnary creates a single interceptor out of a chain of many interceptors.
